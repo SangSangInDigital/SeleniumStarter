6c6ff22b4ffdc484d3e398c0bd49cf6f589bd582
soomin-jin <sm.jin@sangsanginib.com>
2023-06-27 08:16:17 +0900

    align code

--- src/main/java/com/sangsanginib/seleniumstarter/service/CrawlingService.java
+++ src/main/java/com/sangsanginib/seleniumstarter/service/CrawlingService.java
@@ -19,9 +19,10 @@ import java.util.List;
 public class CrawlingService {
     private Logger logger = LoggerFactory.getLogger(CrawlingService.class);
     private final CrawlingRepository crawlingRepository;
+
     @Transactional(readOnly = true)
-    public void crawlerPage01 (ChromeDriver driver){
-        try{
+    public void crawlerPage01(ChromeDriver driver) {
+        try {
 
             // 크롤링 작업 수행
             logger.info("****remoteDriver1****");
@@ -35,7 +36,7 @@ public class CrawlingService {
             List<WebElement> webElements = driver.findElements(By.xpath("//*[@id=\"content\"]/div[2]/div/div[2]/table/tbody/tr"));
 
             int size = webElements.size();
-            logger.info("****size: "+size);
+            logger.info("****size: " + size);
             logger.info("****Before crawling****");
             for (int i = 1; i < size + 1; i += 2) {
                 CrawlingData data = new CrawlingData();
@@ -53,7 +54,7 @@ public class CrawlingService {
             logger.info("****After beforeReturnRate****");
             logger.info("****Before afterReturnRate****");
             for (int i = 2; i < size + 2; i += 2) {
-                String afterReturnRate = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[" + i +"]/td[3]")).getText();
+                String afterReturnRate = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[" + i + "]/td[3]")).getText();
                 logger.info(afterReturnRate);
             }
             logger.info("****After afterReturnRate****");
@@ -66,18 +67,25 @@ public class CrawlingService {
             logger.error("====한국투자증권 crawling error end====");
         }
     }
+
     @Transactional(readOnly = true)
-    public void crawlerPage02 (ChromeDriver driver){
+    public void crawlerPage02(ChromeDriver driver) {
         driver.get("https://www.shinhansec.com/siw/wealth-management/bond-rp/5901/view.do");
-    }@Transactional(readOnly = true)
-    public void crawlerPage03 (ChromeDriver driver){
-        driver.get("https://www.kbsec.com/go.able?linkcd=s010602010000");
-    }@Transactional(readOnly = true)
-    public void crawlerPage04 (ChromeDriver driver){
+    }
+    
+    @Transactional(readOnly = true)
+    public void crawlerPage03(ChromeDriver driver) {
         driver.get("https://www.samsungpop.com/?MENU_CODE=M1231752589437");
-    }@Transactional(readOnly = true)
-    public void crawlerPage05 (ChromeDriver driver){
+    }
+
+    @Transactional(readOnly = true)
+    public void crawlerPage04(ChromeDriver driver) {
         driver.get("https://securities.miraeasset.com/hks/hks4036/r01.do");
+    }
+
+    @Transactional(readOnly = true)
+    public void crawlerPage05(ChromeDriver driver) {
+        driver.get("https://www.kbsec.com/go.able?linkcd=s010602010000");
     }
 
 }
