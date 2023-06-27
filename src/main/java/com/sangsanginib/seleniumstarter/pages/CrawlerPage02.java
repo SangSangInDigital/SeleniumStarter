609e7a0f592ab6b1012d098d08c9277143801076
soomin-jin <sm.jin@sangsanginib.com>
2023-06-27 09:20:55 +0900

    code align

--- src/main/java/com/sangsanginib/seleniumstarter/pages/CrawlerPage02.java
+++ src/main/java/com/sangsanginib/seleniumstarter/pages/CrawlerPage02.java
@@ -20,22 +20,7 @@ public class CrawlerPage02 {
         ChromeDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.shinhansec.com/siw/wealth-management/bond-rp/5901/view.do");
 
-        // String title = driver.getTitle();
-        // System.out.println(title);
-        // assertEquals("Web form", title);
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        // WebElement textBox = driver.findElement(By.name("my-text"));
-        // WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        // textBox.sendKeys("Selenium");
-        // submitButton.click();
-
-        // List<BondsList> bondsLists = new ArrayList<>();
-        //*[@id="main"]/table/tbody/tr[1]/td[2]/a
-        //*[@id="main"]/table/tbody/tr[2]/td[2]/a
-        //*[@id="main"]/button//*[@id="main"]/button
 
         int maxAttempts = 10;
         int attemptCount = 0;
@@ -66,20 +51,6 @@ public class CrawlerPage02 {
             System.out.println(title);//*[@id="content"]/div[2]/div/div[2]/table/tbody/tr[102]/td[2] //*[@id="content"]/div[2]/div/div[2]/table/tbody/tr[102]/td[3] //*[@id="content"]/div[2]/div/div[2]/table/tbody/tr[101]/td[2]/strong
             String endDate = driver.findElement(By.xpath("//*[@id=\"main\"]/table/tbody/tr[" + i + "]/td[3]")).getText();
             System.out.println(endDate);
-            // /html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]
-            // /html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[12]/td[2]
-            // /html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[14]/td[2]
-            // String afterReturnRate = "";
         }
-
-        /*for (int i = 2; i < size + 2; i += 2) {
-            String beforeReturnRate = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[" + i + "]/td[2]")).getText();
-            System.out.println(beforeReturnRate);
-        }
-
-        for (int i = 2; i < size + 2; i += 2) {
-            String afterReturnRate = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[" + i +"]/td[3]")).getText();
-            System.out.println(afterReturnRate);
-        }*/
     }
 }
